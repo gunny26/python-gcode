cb222d8eebdf0fd1162aac215b98c045fb69da3e
Arthur Messner <arthur.messner@gmail.com>
2014-03-02 22:12:24 +0100

    use GpioObjects instead of real RPi.GPIO

--- LaserEngraver.py
+++ LaserEngraver.py
@@ -19,11 +19,14 @@ import sys
 import logging
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 try:
-    import RPi.GPIO as GPIO
+    #import RPi.GPIO as GPIO
+    from GpioObject import GpioObject
+    GPIO = GpioObject(GpioObject.BOARD)
 except ImportError:
     logging.error("Semms not to be a RaspberryPi")
     from  FakeGPIO import FakeGPIO as GPIO
 # own modules
+from FakeGPIO import GPIOWrapper as gpio
 #from GcodeGuiTkinter import GcodeGuiTkinter as GcodeGuiTkinter
 #from GcodeGuiPygame import GcodeGuiPygame as GcodeGuiPygame
 #from GcodeGuiPygame import GcodeGuiPygame as GcodeGuiPygame
@@ -40,50 +43,85 @@ from Transformer import Transformer as Transformer
 def main(): 
     if len(sys.argv) == 1:
         sys.argv.append("examples/tiroler_adler.ngc")
+    # STEP 1 - GPIO Initialization
     # bring GPIO to a clean state
-    GPIO.cleanup()
-    GPIO.setmode(GPIO.BCM)
     try:
-        logging.info("Initialize GPIO")
-        # enable pin for L293D IC
-        GPIO.setup(23, GPIO.OUT)
-        GPIO.output(23, 1)
-        # laser power off
-        GPIO.setup(14, GPIO.OUT)
-        GPIO.output(14, 0)
-        # build our controller
-        logging.info("Creating Controller Object")
-        controller = Controller(resolution=512/36, default_speed=1.0, delay=0.0)
-        controller.add_motor("X", BipolarStepperMotor(coils=(4, 2, 27, 22), max_position=512, min_position=0, delay=0.0))
-        controller.add_motor("Y", BipolarStepperMotor(coils=(24, 25, 7, 8), max_position=512, min_position=0, delay=0.0))
-        controller.add_motor("Z", LaserMotor(laser_pin=14, min_position=-10000, max_position=10000, delay=0.0))
-        controller.add_spindle(Spindle())
-        controller.add_transformer(Transformer())
-        # create parser
-        logging.info("Creating Parser Object")
-        parser = Parser(filename=sys.argv[1])
-        parser.set_controller(controller)
-        # create gui
-        logging.info("Creating GUI")
-        gui = LaserSimulator(automatic=True, zoom=10.0, controller=controller, parser=parser)
-        # gui = GcodeGuiConsole()
-        controller.set_gui_cb(gui.controller_cb)
-        parser.set_gui_cb(gui.parser_cb)
-        # start
-        logging.info("Please move stepper motors to origin (0, 0, 0)")
-        #key = raw_input("Press any KEY when done")
+        GPIO.cleanup_existing()
+    except AttributeError:
+        pass
+    GPIO.setmode(GPIO.BOARD)
+    # define GPIO Pins to use
+    enable_pin = gpio(23, GPIO)
+    enable_pin.setup(GPIO.OUT)
+    laser_pin = gpio(14, GPIO)
+    laser_pin.setup(GPIO.OUT)
+    m_a_a1 = gpio(4, GPIO)
+    m_a_a1.setup(GPIO.OUT)
+    m_a_a2 = gpio(2, GPIO)
+    m_a_a2.setup(GPIO.OUT)
+    m_a_b1 = gpio(27, GPIO)
+    m_a_b1.setup(GPIO.OUT)
+    m_a_b2 = gpio(22, GPIO)
+    m_a_b2.setup(GPIO.OUT)
+    m_b_a1 = gpio(24, GPIO)
+    m_b_a1.setup(GPIO.OUT)
+    m_b_a2 = gpio(25, GPIO)
+    m_b_a2.setup(GPIO.OUT)
+    m_b_b1 = gpio(7, GPIO)
+    m_b_b1.setup(GPIO.OUT)
+    m_b_b2 = gpio(8, GPIO)
+    m_b_b2.setup(GPIO.OUT)
+    logging.info("Initialize GPIO")
+    # enable pin for L293D Chip
+    enable_pin.output(1)
+    # laser power off
+    laser_pin.output(0)
+    # STEP 2 - create Objects and connect them
+    # build our controller
+    logging.info("Creating Controller Object")
+    controller = Controller(resolution=512/36, default_speed=1.0, autorun=False)
+    controller.add_motor("X", BipolarStepperMotor(coils=(m_a_a1, m_a_a2, m_a_b1, m_a_b2), max_position=512, min_position=0, delay=0.002))
+    controller.add_motor("Y", BipolarStepperMotor(coils=(m_b_a1, m_b_a2, m_b_b1, m_b_b2), max_position=512, min_position=0, delay=0.002))
+    controller.add_motor("Z", LaserMotor(laser_pin=laser_pin, min_position=-10000, max_position=10000, delay=0.00))
+    controller.add_spindle(Spindle())
+    controller.add_transformer(Transformer())
+    # create parser
+    logging.info("Creating Parser Object")
+    parser = Parser(filename=sys.argv[1], autorun=False)
+    parser.set_controller(controller)
+    # create gui
+    logging.info("Creating GUI")
+    gui = LaserSimulator(automatic=True, zoom=10.0, controller=controller, parser=parser)
+    # gui = GcodeGuiConsole()
+    controller.set_gui_cb(gui.controller_cb)
+    parser.set_gui_cb(gui.parser_cb)
+    # start
+    logging.info("Please move stepper motors to origin (0, 0, 0)")
+    #key = raw_input("Press any KEY when done")
+    # STEP 3 - run 
+    # this is usually done from
+    try:
         parser.read()
+        #key = raw_input("Parsing done, press Return to call controller")
+        parser.run()
+        #key = raw_input("Controller calculations done, press Return to move")
+        controller.run()
+        key = raw_input("Controller calculations done, press Return to move")
     except ControllerExit as exc:
         logging.info(exc)
     except Exception as exc:
         logging.exception(exc)
-    GPIO.output(23, 0)
-    GPIO.output(14, 0)
+    except KeyboardInterrupt as exc:
+        logging.exception(exc)
+    # STEP 4 - shutdown
+    # inidcate gui to quit
+    gui.stop_flag = True
+    # cleanup gpio
     GPIO.cleanup()
 
 if __name__ == "__main__":
-    #main()
-    #sys.exit(0)
+    main()
+    sys.exit(0)
     import cProfile
     import pstats
     profile = "Tracer.profile"
