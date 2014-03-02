10a2113a631963168a70fd77fd6044eda8126229
Arthur Messner <arthur.messner@gmail.com>
2014-03-02 22:13:14 +0100

    Code cleanup and comments and autorun

--- Parser.py
+++ Parser.py
@@ -14,11 +14,18 @@ import re
 
 class Parser(object):
     """
-    Class to parse GCode Text Commands
+    Class to parse GCode Commands from File
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename, autorun=True):
+        """
+        @params
+        filename -> filename to read G-Codes from
+        autorun -> should controller method calles automatically be initialized
+            after parssing of file is done, or not
+        """
         self.filename = filename
+        self.autorun = autorun
         # last known g code
         self.last_g_code = None
         # precompile regular expressions
@@ -49,9 +56,9 @@ class Parser(object):
         """
         calls G- or M- code Method
 
-        if no G-Code Method was given, the last methos will be repeated
+        if no G-Code Method was given, the last method will be repeated
 
-        fo example G02 results in call of self.controller.G02(args)
+        for example G02 results in call of self.controller.G02(args)
         """
         # logging.debug("calling %s(%s)", methodname, args)
         self.command = "%s(%s)" % (methodname, args)
@@ -63,13 +70,19 @@ class Parser(object):
         if methodname[0] == "G":
             self.last_g_code = methodname
 
+    def run(self):
+        """run stored methodcalls to controller in batch"""
+        for (method_to_call, args, methodname) in self.calls:
+            # logging.info("calling %s(%s)", methodname, args)
+            method_to_call(args)
+
     def read(self):
         """
         read input file line by line, and parse gcode Commands
         """
         fd = os.open(self.filename, os.O_RDONLY)
-        f = os.fdopen(fd, "r", -1)
-        for line in f.readlines():
+        f = os.fdopen(fd, "r")
+        for line in f:
             # cleanup line
             line = line.strip()
             line = line.upper()
@@ -113,6 +126,7 @@ class Parser(object):
             if len(remaining_line) > 0:
                 logging.info("remaining: %s", line)
         logging.info("parsing done")
-        for (method_to_call, args, methodname) in self.calls:
-            logging.debug("calling %s(%s)", methodname, args)
-            method_to_call(args)
+        if self.autorun is True:
+            self.run()
+        else:
+            logging.info("You have to call run, to call Controller methods")
