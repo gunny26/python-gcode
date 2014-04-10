ceb21e85a4b2131ff4b85f79efad6625815e5d17
Arthur Messner <arthur.messner@gmail.com>
2014-04-10 23:45:47 +0200

    code cleanup

--- src/GPIOObject/__init__.pyx
+++
@@ -1,5 +0,0 @@
-#/usr/bin/python
-
-#from GPIOObject import GPIOObject as GPIOObject
-#import FakeGPIO
-#from GPIOWrapper import GPIOWrapper
--- src/Spindle/Spindle.pyx
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-from Spindle import Spindle as Spindle
-from Laser import Lase as Laser
